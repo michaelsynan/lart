37f8ae0ad3bb62248751cae2e5d4304de7fb2a53
mikey <mforest@protonmail.com>
2023-10-24 17:42:42 -0400

    Contact form updates

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,30 +7,30 @@ definePageMeta({
 <template>
   <div class="w-full flex justify-center">
     <div class="relative flex flex-col items-center justify-start md:mt-0 mt-20 md:justify-center h-screen w-full">
-      <section class="align-left w-full">
-        <div class="mx-auto w-full md:w-2xl px-4">
-          <h2 class="mb-4 text-5xl md:text-6xl tracking-tight font-extrabold text-left md:text-center text-white px-0 pb-4 md:px-4">Get In Touch</h2>
-          <form action="#" class="space-y-8 text-lg">
+      <section class="align-left w-full mb-0 md:mb-10">
+        <div class="mx-auto w-full md:w-xl px-4">
+          <h2 class="mb-4 text-5xl md:text-5xl tracking-tight font-extrabold text-left md:text-center text-white px-0 pb-3 md:px-4">Get In Touch</h2>
+          <form action="#" class="space-y-6 text-lg">
             <div>
-              <label for="email" class="block mb-2 ttext-gray-300 text-left font-bold">Email</label>
+              <label for="email" class="block mb-2 text-gray-300 text-left font-bold">Email</label>
               <input type="email" id="email"
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900  rounded-sm focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                class="text-base shadow-sm bg-gray-50 border border-gray-300 text-gray-900  rounded-sm focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="your@email.com" required>
             </div>
             <div>
               <label for="subject" class="block mb-2  font-bold text-gray-300 text-left">Subject</label>
               <input type="text" id="subject"
-                class="block p-3 w-full text-gray-900 bg-gray-50 rounded-sm border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                class="text-base block p-3 w-full text-gray-900 bg-gray-50 rounded-sm border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Let us know how we can help you" required>
             </div>
             <div class="sm:col-span-2">
               <label for="message" class="block mb-2 font-bold text-gray-400 text-left">Message</label>
               <textarea id="message" rows="6"
-                class="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-sm shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                class="text-base block p-2.5 w-full text-gray-900 bg-gray-50 rounded-sm shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Send a detailed message..."></textarea>
             </div>
             <button type="submit"
-              class="uppercase tracking-wide inline-flex justify-between w-full py-3 px-5 text-2xl font-bold text-center text-white rounded-sm bg-primary-700  hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 bg-teal dark:focus:ring-primary-800">
+              class="uppercase tracking-wide inline-flex justify-between w-full py-3 px-5 text-xl font-bold text-center text-white rounded-sm bg-primary-700  hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 bg-teal dark:focus:ring-primary-800">
               <div class="w-full">Send</div>
             </button>
           </form>
