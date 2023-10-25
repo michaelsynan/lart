9f7f23b0ece1116c46c7a5617beb2c220d4637bb
mikey <mforest@protonmail.com>
2023-10-25 06:48:10 -0400

    Contact form updates

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,18 +1,36 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+
 definePageMeta({
   layout: 'default'
 })
 
 const loading = ref(false);
+const response = ref('');
+const formSent = ref(false); // New variable to track if form is sent successfully
 
 const handleButtonClick = () => {
+  if (formSent.value) return; // Prevent resending if form already sent
+
   loading.value = true;
+
+  // Simulating a network call to send the email
   setTimeout(() => {
     loading.value = false;
+
+    // Assuming success for now; later, you'd check the result of your actual mailer function.
+    const isSuccess = true; // replace with actual success condition
+
+    if (isSuccess) {
+      response.value = 'Success!';
+      formSent.value = true; // Set formSent to true on success
+    } else {
+      response.value = 'Something went wrong';
+    }
   }, 2000);
 };
 </script>
+
 
 <template>
   <div class="w-full flex justify-center">
@@ -41,23 +59,24 @@ const handleButtonClick = () => {
                 class="text-base block p-2.5 w-full text-gray-900 bg-gray-50 rounded-sm shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Send a detailed message..."></textarea>
             </div>
-            <button type="submit" 
-        @click="handleButtonClick"
-        class="uppercase tracking-wide flex justify-center items-center w-full h-12 text-xl font-bold text-center text-white rounded-sm  focus:ring-4 focus:outline-none focus:ring-primary-300 bg-teal hover:bg-teal-500 transition-color duration-100 ease-in-out dark:focus:ring-primary-800">
-  
+            <button type="submit" @click="handleButtonClick" :disabled="formSent"
+  :class="{
+    'cursor-not-allowed bg-opacity-50': formSent,
+    'hover:bg-teal-500': !formSent
+  }"
+  class="uppercase tracking-wide flex justify-center items-center w-full h-12 text-xl font-bold text-center text-white rounded-sm focus:ring-4 focus:outline-none focus:ring-primary-300 bg-teal transition-color duration-100 ease-in-out dark:focus:ring-primary-800">
+ 
   <span v-if="!loading" class="w-full text-center">Send</span>
-
-  <div v-if="loading" class="flex items-center justify-center">
-    <svg xmlns="http://www.w3.org/2000/svg" 
-         class="animate-spin h-6 w-6" 
-         viewBox="0 0 24 24" 
-         style="transform-origin: center;">
-      <path fill="white" d="M12 4V2A10 10 0 0 0 2 12h2a8 8 0 0 1 8-8Z" />
-    </svg>
-  </div>
-  
-</button>
-
+              <div v-if="loading" class="flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="animate-spin h-6 w-6" viewBox="0 0 24 24"
+                  style="transform-origin: center;">
+                  <path fill="white" d="M12 4V2A10 10 0 0 0 2 12h2a8 8 0 0 1 8-8Z" />
+                </svg>
+              </div>
+            </button>
+            <div class="h-6">
+              <div v-if="response">{{ response }}</div>
+            </div>
           </form>
         </div>
       </section>
