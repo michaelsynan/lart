a2f329f07f336c2c060b9a1c770f2da342641c8d
mikey <mforest@protonmail.com>
2023-10-25 09:42:51 -0400

    updated font styles

--- pages/about.vue
+++ pages/about.vue
@@ -3,9 +3,10 @@ definePageMeta({
   layout: 'default'
 })
 </script>
+
 <template>
   <div class="w-full flex justify-center px-4">
-  <div class="relative flex flex-col items-center justify-center h-screen">
+    <div class="relative flex flex-col items-center justify-start md:mt-0 mt-20 md:justify-center h-screen">
     <div class="border-4 border-teal mb-10 rounded-md bg-teal w-full">
     <h1 class="text-4xl md:text-6xl font-bold pb-4 px-6 md:px-10 py-4 md:py-6 tracking-wide md:tracking-wider rounded bg-stone-950 w-full mainheading">L'art de la rue</h1>
     <h2 class="text-lg md:text-xl w-full bg-teal w-full font-bold py-2 uppercase mainheading"><span class="">Established 2000</span></h2>
